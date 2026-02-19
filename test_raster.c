912fa938346e0a1ea1587d884cd4a188eefc658f
sfaza339 <144830587+sfaza339@users.noreply.github.com>
2026-02-19 10:27:52 -0700

    Update test_raster.c

--- test_raster.c
+++ test_raster.c
@@ -1,1 +1,21 @@
+#include <osbind.h>
+#include "raster.h"
 
+int main()
+{
+    void *base = Physbase();
+
+    /*
+    plot_something_1(base, 100, 100);
+    plot_something_2(base, 200, 200);
+    */
+
+    /* clear_screen */
+    clear_screen(base);
+
+    /* plot_pixel */
+    
+    return 0;
+}
+
+test_raster.c
