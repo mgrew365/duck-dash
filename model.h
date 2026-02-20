d0eef41ded741086b21ba49d3e640c781c7be051
mgrew365 <mgrew365@mtroyal.ca>
2026-02-20 14:41:15 -0700

    basic bitmap constants?

--- model.h
+++ model.h
@@ -1,4 +1,17 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#include "types.h"
+
+/* STRUCTURES*/
+
+/* BITMAPS*/
+extern const UINT16 duck[];
+extern const UINT16 building[];
+
+/* PROTOTYPES*/
+
+
+
+
 #endif
