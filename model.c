657caf72b24598d8f8a237c8ec3d6e89d9e8f59a
sfaza339 <144830587+sfaza339@users.noreply.github.com>
2026-02-20 16:39:51 -0700

    Started model.c, unsure on how to start the functions move_buildings and init_model

--- model.c
+++ model.c
@@ -1,2 +1,33 @@
 #include <osbind.h>
 #include "model.h"
+
+/* Leave extra space at the bottom of screen so duck doesnt land on edge of screen, lands on floor */
+#define FLOOR_BUFFER 40
+
+/* Ducks movement (jump/fall)*/
+void  move_duck(Duck *duck) {
+    /* Ensure duck stays in the screen*/
+    if (duck->y < 0) {
+        duck->y = 0;
+    }
+
+    if (duck->y > 400 - FLOOR_BUFFER) {
+        duck->y = 400 - FLOOR_BUFFER;
+    }
+    
+}
+/* Move buildings left towards the duck*/
+/*idek how to start this*/
+void move_buildings(Building buildings[], int count) {
+
+}
+
+/* Initialize the model to default game state */
+void init_model(Model *model) {
+
+}
+
+/* Increment score */
+void increment_score(Model *model) {
+    model->score++;
+}
