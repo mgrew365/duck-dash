352be569937536bc92ff8a1413ac8496228890fd
sfaza339 <144830587+sfaza339@users.noreply.github.com>
2026-02-20 16:26:38 -0700

    Implemented necessary logic for model.h to then be implemented in model.c

--- model.h
+++ model.h
@@ -3,15 +3,46 @@
 
 #include "types.h"
 
-/* STRUCTURES*/
+/* PROTOTYPES*/
 
+/* Duck Object*/
+typedef struct {
+    unsigned int x, y;
+    int delta_y;
+} Duck;
+
+/* Building Object */
+typedef struct {
+    unsigned int x, y;
+    unsigned int width;
+    unsigned int height;
+} Building;
+
+/* Main Game Model */
+#define MAX_BUILDINGS 3 /* Maximum number of buildings on the screen (change as needed)*/
+
+typedef struct {
+    Duck duck;
+    Building buildings[MAX_BUILDINGS];
+    unisgned int score;
+} Model;
+
+/* STRUCTURES*/
 /* BITMAPS*/
 extern const UINT16 duck[];
 extern const UINT16 building[];
 
-/* PROTOTYPES*/
+/* Ducks movement (jump/fall)*/
+void move_duck(Duck *duck);
 
+/* Move buildings left towards the duck*/
+void move_buildings(Building buildings[], int count);
 
+/* Initialize the model to default game state */
+void init_model(Model *model);
+
+/* Increment score */
+void increment_score(Model *model);
 
 
 #endif
