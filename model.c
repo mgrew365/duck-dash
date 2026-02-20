4b2a6d057ba73c73a10f5f3c60399c108571ba1c
sfaza339 <144830587+sfaza339@users.noreply.github.com>
2026-02-20 16:44:59 -0700

    Started model.c

--- model.c
+++ model.c
@@ -4,8 +4,14 @@
 /* Leave extra space at the bottom of screen so duck doesnt land on edge of screen, lands on floor */
 #define FLOOR_BUFFER 40
 
+/* What is the actual size of the duck?*/
+#define DUCK_WIDTH 32
+#define DUCK_HEIGHT 32
+
+/* Maybe we add a helper function to help detect collisions and call this function within move_duck*/
+
 /* Ducks movement (jump/fall)*/
-void  move_duck(Duck *duck) {
+void move_duck(Duck *duck) {
     /* Ensure duck stays in the screen*/
     if (duck->y < 0) {
         duck->y = 0;
