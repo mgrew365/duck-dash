5655150f05856459bd4fc51b19c4efb9ee6c67cf
sfaza339 <144830587+sfaza339@users.noreply.github.com>
2026-02-20 22:43:50 -0700

    Created synch.c with implemented logic up to a certain extent

---
+++ synch.c
@@ -0,0 +1,36 @@
+#include "synch.h"
+
+#define TICKS_PER_SECOND 70
+
+#define SPEED_UP_1 20
+#define SPEED_UP_2 40
+#define SPEED_UP_3 80
+
+/* Building appearing gradually with speed of appearence increasing over time 
+   This function is not complete at the moment as we cant actually test this aspect yet
+*/
+void building_appearance(Model *model, unsigned int elapsed_ticks) {
+    (void)model;
+    (void)elapsed_ticks;
+}
+
+/* Speed increase at intervals: 20s, 40s, 80s, etc */
+void speed_increase(Model *model, unsigned int elapsed_ticks) {
+    unsigned int elapsed_seconds = elapsed_ticks / TICKS_PER_SECOND;
+    unsigned int i;
+
+    if (elapsed_seconds == SPEED_UP_1 ||
+        elapsed_seconds == SPEED_UP_2 ||
+        elapsed_seconds == SPEED_UP_3)
+    {
+        for (i = 0; i < MAX_BUILDINGS; i++) {
+            /* Increase speed by making delta_x more negative */
+            model->buildings[i].delta_x -= 1;
+        }
+    }
+}
+  t
+/* Score updates based on elapsed gameplay time */
+void update_score(Model *model, unsigned int elapsed_ticks) {
+    model->score = elapsed_ticks / TICKS_PER_SECOND;
+}
