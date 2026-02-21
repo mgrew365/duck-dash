c7dfba62bab042f8d787e95efd60509185896242
sfaza339 <144830587+sfaza339@users.noreply.github.com>
2026-02-20 22:27:06 -0700

    Create asynch.c

---
+++ asynch.c
@@ -0,0 +1,16 @@
+#include "asynch.h"
+
+/* Duck Jump - triggered when up key is pressed */
+void duck_jump(Model *model) {
+    /* Only jump if duck is not already moving upward */
+    if (model->duck.delta_y >= 0) {
+        model->duck.delta_y = -8;   /* jump strength (change as need be) */
+    }
+}
+/* Quit game - triggered when ESC key is pressed 
+   This function is not complete at the moment as we cant actually test this aspect yet
+*/
+
+void quit_game(Model *model) {
+    (void)model; 
+}
