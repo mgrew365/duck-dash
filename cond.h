fc77332db1c7286d811d4607d945b90d2a0bd5b3
sfaza339 <144830587+sfaza339@users.noreply.github.com>
2026-02-22 16:48:16 -0700

    Handles bool functions

--- cond.h
+++ cond.h
@@ -2,19 +2,24 @@
 #define COND_H
 
 #include "model.h"
-#include <stdbool.h>
+
+/* Define bool */
+#ifndef BOOL_DEFINED
+#define BOOL_DEFINED
+typedef enum { false = 0, true = 1 } bool;
+#endif
 
 /* Duck collides with building resulting in game over */
 bool duck_building_collision(const Model *model);
 
-/* Building collides with left border to be removed from the screen */
-bool building_left_corder_collision(const Model *model);
+/* Building collides with left border to be rem oved from the screen */
+bool building_left_border(const Building *building);
 
 /* Duck reaches its maximum jump height and starts to descend afterwords */
-bool duck_max_height(const Model *model);
+bool duck_max_height(const Duck *duck);
 
 /* Duck collides with ground stopping descent */
-bool duck_ground_collision(const Model *model);
+bool duck_ground_collision(const Duck *duck);
 
 /* Restart game when gameplay has ended */
 bool restart_game(const Model *model);
