c75d694ef5bcf13f0d1e2178f6ba88416a8381d2
sfaza339 <144830587+sfaza339@users.noreply.github.com>
2026-02-22 20:33:09 -0700

    Updated cond.h with comments explaining the function

--- cond.h
+++ cond.h
@@ -9,19 +9,59 @@
 typedef enum { false = 0, true = 1 } bool;
 #endif
 
-/* Duck collides with building resulting in game over */
+
+/* ----- Function: duck_building_collision -----
+
+Purpose: This function checks if the duck has colliding with the building. If the output is true, game over. 
+
+Input: Model *model: A pointer to the game's current model
+
+Output: boolean value: true if collision occurs, else false
+*/
 bool duck_building_collision(const Model *model);
 
-/* Building collides with left border to be rem oved from the screen */
+
+/* ----- Function: building_left_border -----
+
+Purpose: This function checks if the building has collided with the left border of the screen to then be removed from the screen
+         if the output is true.
+
+Input: Building *building: A pointer to the Building object
+
+Output: boolean value: true if building is of the screen, else false
+*/
 bool building_left_border(const Building *building);
 
-/* Duck reaches its maximum jump height and starts to descend afterwords */
+
+/* ----- Function: duck_max_height -----
+
+Purpose: This function checks if the duck has reached its maximum jump height for it then to descend.
+
+Input: Duck *duck: pointer to Duck object 
+
+Output: boolean value: true if duck has reached the maximum jump height, else false
+*/
 bool duck_max_height(const Duck *duck);
 
-/* Duck collides with ground stopping descent */
+
+/* ----- Function: duck_ground_collision -----
+
+Purpose: This function checks if the duck has collided with the ground.
+
+Input: Duck *duck: pointer to Duck object
+
+Output: boolean value: true if duck has hit the ground, else false
+*/
 bool duck_ground_collision(const Duck *duck);
 
-/* Restart game when gameplay has ended */
+/* ----- Function: restart_game -----
+
+Purpose: This function determines if a game should be restarted.
+
+Input: Model *model: A pointer to current game model
+
+Output: boolean value: boolean value: true if the game has ended and should be restarted, else false
+*/
 bool restart_game(const Model *model);
 
 #endif 
