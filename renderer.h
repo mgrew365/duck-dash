990151e976f719f402c65bb81da77cd5ad06bee7
mgrew365 <mgrew365@mtroyal.ca>
2026-02-23 01:54:44 -0700

    render.h documentation

--- renderer.h
+++ renderer.h
@@ -3,8 +3,10 @@ File: RENDERER.H
 Names: Manroop Grewal, Sarah Fazal
 Instructor: Steve Kalmar
 Assignment: Checkpoint 2 - COMP 2659 
-Date Modified: February 21, 2026
-File Description: 
+Date Modified: February 23, 2026
+File Description: Defines headers and interface for the renderer
+module. It is part of drawing the game objects from the game model
+to the screen using rasters.
 
 */
 
@@ -14,9 +16,41 @@ File Description:
 #include "model.h"
 #include "types.h"
 
+/*
+Function: Render
+Purpose: Renders the full frame of the game state at the current state
+of the model.
+Input: Model Pointer: the current game state to be rendered.
+       Address(UINT8*): pointer to the start fo the memory of the frame buffer
+Output: None/No direct value
+*/
 void render(const Model *model, UINT8 *base);
+
+/*
+Function: Render_Duck
+Purpose: Draws the duck object to the screen. Based on its current position.
+Input: Duck Pointer: the object to render
+       Address(UINT8*): pointer to start of FB
+Output: None/No direct value
+*/
 void render_duck(const Duck *duck, UINT8 *base);
+
+/*
+Function: Render_Building
+Purpose: Draws the building object to the screen at its current psoition.
+Input: Building Pointer: the object to render
+       Address(UINT8*): pointer to start of FB
+Output: None/No direct value
+*/
 void render_building(const Building *b, UINT8 *base);
+
+/*
+Function: Render_score
+Purpose: Draw the score and score values as the text to the screen
+Input: score(unsigned int) value getting displayed
+       Address(UINT8*): pointer to start of FB
+Output: None/No direct value
+*/
 void render_score(unsigned int score, UINT8 *base);
 
 #endif
