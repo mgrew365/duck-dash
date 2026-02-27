5fb405a9e6447765d2e73576f635956e8f3eb38a
sfaza339 <144830587+sfaza339@users.noreply.github.com>
2026-02-26 20:19:22 -0700

    Updated model.c with documentation

--- model.c
+++ model.c
@@ -1,9 +1,22 @@
+/*
+File: MODEL.H
+Names: Manroop Grewal, Sarah Fazal
+Instructor: Steve Kalmar
+Assignment: Checkpoint 2 - COMP 2659 
+Date Modified: February 21, 2026
+File Description: This file defines the game model and basic movement functions required for Duck Dash.
+                  It includes a test snapshot of the intial game state for testing purposes,
+                  fucntions to move the duck and buildings during gameplay and a function to create
+                  an initial model state. Together, these functions modify or return the game model to update 
+                  game state in response to asyncronous, conditional or synchronous events
+*/
 #include <osbind.h>
 #include "model.h"
 
 /* Leave extra space at the bottom of screen so duck doesnt land on edge of screen, lands on floor */
 #define FLOOR_BUFFER 40
 
+/* Initial game snapshot of game state for testing purposes*/
 Model testDuckDashSnapshot = {
     /* Duck */
     {
@@ -24,11 +37,29 @@ Model testDuckDashSnapshot = {
     0
 };
 
-/* Ducks movement (jump/fall)*/
+/* ----- Function: move_duck -----
+
+Purpose: Updates the vertical position of the duck.
+
+Input: Duck *duck: a pointer to duck object
+
+Output: None
+*/
 void move_duck(Duck *duck) {
     duck->y += duck->delta_y;
 }
-/* Move buildings left towards the duck*/
+
+
+/* ----- Function: move_buildings -----
+
+Purpose: Updates the horizontal position of the buildings and moves them left towards
+         the duck.
+
+Input: buildings[]: an array of building objects
+       count: number of buildings in the array
+
+Output: None
+*/
 void move_buildings(Building buildings[], unsigned int count) {
     unsigned int i;
 
@@ -37,7 +68,14 @@ void move_buildings(Building buildings[], unsigned int count) {
     }
 }
 
-Model model_create_initial()
-{
+/* ----- Function: model_create_initial -----
+
+Purpose: This function returns a copy of the initial game model for testing or restarting the game. 
+
+Input: None
+
+Output: Model object which contains initial duck, building position, and score
+*/
+Model model_create_initial() {
     return testDuckDashSnapshot;
 }
