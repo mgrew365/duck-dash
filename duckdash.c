a091057393e0a5e05139152427e01fdbdf112f2f
sfaza339 <144830587+sfaza339@users.noreply.github.com>
2026-03-09 21:16:41 -0600

    Create duckdash.c as main game model. Still must be implemented

---
+++ duckdash.c
@@ -0,0 +1,13 @@
+#include "model.h"
+#include "renderer.h"
+#include "input.h"
+#include "asynch.h"
+#include "synch.h"
+#include "cond.h"
+
+#define CLOCK_ADDRESS 0x462
+
+int main() {
+
+    return 0;
+}
