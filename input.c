70fd056c6443dc93792db0eeb1f4a71204da37cd
mgrew365 <mgrew365@mtroyal.ca>
2026-03-11 12:28:07 -0600

    Created input.c

---
+++ input.c
@@ -0,0 +1,19 @@
+/*
+File: INPUT.c
+Names: Manroop Grewal, Sarah Fazal
+Instructor: Steve Kalmar
+Assignment: Checkpoint 3 - COMP 2659 
+Date Modified: March 10, 2026
+File Description: This input files ocntains the logic from the functions defined in input.h
+*/
+
+/* ----- Function: has_input -----
+Purpose: Checks if a key has been pressed (non-blocking).
+Output: True (1) if a character is waiting in the buffer, else false (0).
+*/
+void has_input();
+/* ----- Function: get_input -----
+Purpose: Retrieves the character from the keyboard buffer.
+Output: The ASCII value of the key pressed.
+*/
+void get_input();
