74cf5e04a33c1ea7a9b8f5ade7aac1dd34c39ce9
sfaza339 <144830587+sfaza339@users.noreply.github.com>
2026-03-15 11:48:36 -0600

    Updated input.h with documentation

--- input.h
+++ input.h
@@ -8,33 +8,34 @@ File Description: This header file defines constants and function prototypes
                   for handling keyboard input in Duck Dash.
 */
 
-
 #ifndef INPUT_H
 #define INPUT_H
 
 #include "types.h"
 #include "events.h"
 
-
 /* Key Constants */
-
 #define ESC_KEY   27    /* ASCII value for the Escape key to trigger quit */
 #define SPACE_BAR 32    /* ASCII value for Space Bar to trigger jump */
 
-
 /* ----- Function: has_input -----
-Purpose: Checks if a key has been pressed (non-blocking).
-Output: True (1) if a character is waiting in the buffer, else false (0).
-*/
 
+Purpose:Checks if a key is waiting in the keyboard buffer.
+
+Input: None
+
+Output: true if a key is available, else false
+*/
 int has_input();
 
-
 /* ----- Function: get_input -----
-Purpose: Retrieves the character from the keyboard buffer.
-Output: The ASCII value of the key pressed.
-*/
 
+Purpose: Gets the next key from the keyboard buffer.
+
+Input: None
+
+Output:ASCII character of key pressed
+*/
 char get_input();
 
 #endif
