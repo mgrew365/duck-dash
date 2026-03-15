0a8a8651642c6713c686f8e7f5cf97853beb537d
sfaza339 <144830587+sfaza339@users.noreply.github.com>
2026-03-15 11:50:10 -0600

    Updated input.c with documentation

--- input.c
+++ input.c
@@ -1,20 +1,27 @@
+/*
+File: INPUT.C
+Names: Manroop Grewal, Sarah Fazal
+Instructor: Steve Kalmar
+Assignment: Checkpoint 3 - COMP 2659 
+Date Modified: March 15, 2026
+File Description:  Contains the input handling functions for DuckDash. This file is
+                   responsible for detecting keyboard input and returning player key
+                   presses so the game can respond to actions such as jumping or quitting.
+
+*/
+
 #include <osbind.h>
 #include <stdio.h>
 #include "input.h"
 
 /* ----- Function: has_input -----
 
-Purpose:
-Checks if a key is waiting in the keyboard buffer.
+Purpose:Checks if a key is waiting in the keyboard buffer.
 
-Input:
-None
+Input: None
 
-Output:
-true  → key available
-false → no key
+Output: true if a key is available, else false
 */
-
 bool has_input(void) {
     return (Cconis() != 0);
 }
@@ -22,16 +29,12 @@ bool has_input(void) {
 
 /* ----- Function: get_input -----
 
-Purpose:
-Gets the next key from the keyboard buffer.
+Purpose: Gets the next key from the keyboard buffer.
 
-Input:
-None
+Input: None
 
-Output:
-ASCII character of key pressed
+Output:ASCII character of key pressed
 */
-
 char get_input(void) {
     return (char)Cnecin();
 }
