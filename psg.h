2aee44214c506f3ecd3d8bc13876c32f8a9729fc
sfaza339 <144830587+sfaza339@users.noreply.github.com>
2026-03-23 21:11:56 -0600

    Create psg.h

---
+++ psg.h
@@ -0,0 +1,22 @@
+#ifndef PSG_H
+#define PSG_H
+
+typedef unsigned char UINT8;
+
+void write_psg(int reg, UINT8 val);
+
+UINT8 read_psg(int reg);
+
+void set_tone(int channel, int tuning);
+
+void set_volume(int channel, int volume);
+
+void enable_channel(int channel, int tone_on, int noise_on);
+
+void stop_sound(void);
+
+void set_noise(int tuning);
+
+void set_envelope(int shape, unsigned int sustain);
+
+#endif
