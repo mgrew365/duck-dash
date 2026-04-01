9d99ee21097c2ac179cfc9776b1f3f6dd7b3fbaa
mgrew365 <mgrew365@mtroyal.ca>
2026-04-01 11:04:45 -0600

    header files and start screen init

--- sscreen.c
+++ sscreen.c
@@ -1,2 +1,62 @@
-#include <osbind.h>   
+#include <stdio.h>
+
 #include "sscreen.h"
+#include "font.c"
+#include "raster.h"
+#include "input.h"
+
+UINT16 capitalD[] = {
+    0xFFF8,
+    0xFFFC,
+    0xFFFE,
+    0xF01F,
+    0xF00F,
+    0xF007,
+    0xF007,
+    0xF007,
+    0xF007,
+    0xF007,
+    0xF007,
+    0xF00F,
+    0xF01F,
+    0xFFFE,
+    0xFFFC,
+    0xFFF8,
+};
+
+int startScreen(UINT8 *base)
+{
+
+    int selection = 0;
+    long input = 0;
+    int result = 0;
+    int i;
+
+    initSplash(base);
+
+    input = 0;
+    while (selection == 0)
+    {
+        input = getKey();
+        if (input != 0)
+        {
+            switch (input)
+            {
+            case '1':
+                selection = 1;
+                break;
+
+            case '2':
+                break;
+
+            case '3':
+            selection = 3;
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+    return selection;
+}
--- sscreen.h
+++ sscreen.h
@@ -1,5 +1,9 @@
 #ifndef SSCREEN_H
 #define SSCREEN_H
 
+#include "font.c"
+
+int startScreen(UINT8 *base);
+int initilizeSplashScreen(UINT8 *base);
 
 #endif
