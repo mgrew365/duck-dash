8f904e283bc8f5d75f1c08adb951216ec31e59b8
sfaza339 <144830587+sfaza339@users.noreply.github.com>
2026-04-02 17:40:32 -0600

    Update music.h

--- music.h
+++ music.h
@@ -4,8 +4,38 @@
 #include "psg.h"
 #include "types.h"
 
+/* ----- Function: start_music -----
+
+Purpose: Initializes and starts the background music by resetting
+         all channel indices, configuring PSG channels, and setting initial tones.
+
+Input: None
+
+Output: None
+*/
 void start_music(void);
 
+
+/* ----- Function: update_music -----
+
+Purpose: Updates the music over time by advancing melody,
+         bass, and rhythm patterns based on elapsed time.
+
+Input: UINT32 time_elapsed: Current system time used to control music timing
+
+Output: None
+*/
 void update_music(UINT32 time_elapsed);
+
+
+/* ----- Function: stop_music -----
+
+Purpose: Stops all sound output by disabling audio channels.
+
+Input: None
+
+Output: None
+*/
+void stop_music(void);
 
 #endif
