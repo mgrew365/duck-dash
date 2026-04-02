a0b9e19d6890e8e37caa0798f030bfe0f370d716
sfaza339 <144830587+sfaza339@users.noreply.github.com>
2026-04-02 16:50:06 -0600

    Update tstspla.c

--- tstspla.c
+++ tstspla.c
@@ -1,11 +1,29 @@
+/*
+File: TSTSPLA.C
+Names: Manroop Grewal, Sarah Fazal
+Instructor: Steve Kalmar
+Assignment: Checkpoint 2 - COMP 2659 
+Date Modified: April 2, 2026
+File Description: This file is used to test the splash game before implementing
+                  it in duckdash.c (main game).
+*/
 #include <osbind.h>
 #include "sscreen.h"
 
-int main()
-{
+/* ----- Function: main -----
+
+Purpose: Entry point for the splash screen test program. It retrieves 
+         the screen base address, calls the splash screen rendering 
+         function, and pauses execution until a key is pressed.
+
+Input: None
+
+Output: int: 0 if program terminated successfully
+*/
+int main() {
 
     UINT32 *base = (UINT32 *)Physbase();
-    initilizeSplashScreen(base);
+    initilize_splash_screen(base);
     
     /* Wait for key press */
     while (!Cconis()) {
